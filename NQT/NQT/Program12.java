53752b80fe87a8de2a5121dbc786e025a7245189
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-04-11 23:49:57 +0530

    recursion

--- NQT/NQT/Program12.java
+++ NQT/NQT/Program12.java
@@ -1,5 +1,20 @@
 package NQT;
 
 public class Program12 {
-    
+    public static int sum = 0;
+
+    public static void sumFirstnatural(int n) {
+        sum = sum + n;
+        n--;
+        
+        if (n == 0) {
+            System.out.println(sum);
+            return;
+        }
+        sumFirstnatural(n);
+    }
+
+    public static void main(String[] args) {
+        sumFirstnatural(5);
+    }
 }
