1f5f75266366ff69382708a1a80914330c1d56b6
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-04-13 21:24:54 +0530

    addFirstnatural

--- NQT/NQT/Program12.java
+++ NQT/NQT/Program12.java
@@ -6,7 +6,7 @@ public class Program12 {
     public static void sumFirstnatural(int n) {
         sum = sum + n;
         n--;
-        
+
         if (n == 0) {
             System.out.println(sum);
             return;
@@ -14,8 +14,18 @@ public class Program12 {
         sumFirstnatural(n);
     }
 
+    public static void addFirstnatural(int i, int sum) {
+        sum = sum + i;
+        if (i == 0) {
+            System.out.println(sum);
+            return;
+        }
+        addFirstnatural(i - 1, sum);
+    }
+
     public static void main(String[] args) {
         sumFirstnatural(5);
-        
+        addFirstnatural(5, 0);
+
     }
 }
