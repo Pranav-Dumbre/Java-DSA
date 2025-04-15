bdfe8eff31572e1e9255ee0130cf826da9d365d3
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-04-15 22:41:40 +0530

    revArray

--- NQT/NQT/Program13.java
+++ NQT/NQT/Program13.java
@@ -1,5 +1,6 @@
 package NQT;
 
+// recursion
 public class Program13 {
     public static int fact(int n) {
         if (n == 0) {
@@ -11,7 +12,27 @@ public class Program13 {
         return n * fact(n - 1);
     }
 
+    public static void revArray(int i, int[] arr, int n) {
+        if (i > n / 2) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+
+        revArray(++i, arr, n);
+    }
+
     public static void main(String[] args) {
         System.out.println(fact(5));
+        System.out.println("________");
+        int arr[] = new int[] { 10, 20, 30, 40, 50 };
+        int x = 10;
+        revArray(0, arr, 5);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+        // System.out.println(x++);
+        // System.out.println(x);
     }
 }
