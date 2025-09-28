bfe0fce2da6188e363e81f5ab9dc2dde13ee0816
Pranav Dumbre <pranavdumbre1911@gmail.com>
2025-09-28 23:51:40 +0530

    Arrays

--- Logic/week_01/Array_02.java
+++ Logic/week_01/Array_02.java
@@ -36,12 +36,32 @@ public class Array_02 {
         }
         System.out.println("Second Largest:" + secondLargest);
     }
+    public static void thirdlargest(int arr[]){
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        int third=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>first){
+                third=second;
+                second=first;
+                first=arr[i];
+            }
+            else if(arr[i]>second && arr[i]!=first){
+                third=second;
+                second=arr[i];
+            }
+            else if(arr[i]>third && arr[i]!=second && arr[i]!=first){
+                third=arr[i];
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 4, 5 };
         findMissingRepeating(arr);
         secondLargest(arr);
-
+        thirdlargest(arr);
     }
 
 }
